8136296ce0ed7611dd558ecd924daf1e8a164c4a
Mac <mac@macs-macbook-pro.local>
2019-01-27 03:33:38 -0600

    Persisting Data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,32 +9,23 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    //make an array of objects
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    
+    //let defaults = UserDefaults.standard
+    
+    //make a data path to our stored data for this app
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-
-        let newItem3 = Item()
-        newItem3.title = "DestroyDemogorgon"
-        itemArray.append(newItem3)
-
-        //if a plist exists with previous added items then get it instead of the itemArray default
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
-            itemArray = items
-        }
-        // Do any additional setup after loading the view, typically from a nib.
+        //declare three Item objects
+        
+        loadItems()
     }
     
-   //MARK - Tableview Datasource Methods
+    //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -44,16 +35,21 @@ class TodoListViewController: UITableViewController {
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         
+        //ternary operator that toggles the check mark on and off
         cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
     //MARK -Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //checkmark if there is none.
         
+        //toggles checkmark status on and off.
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        
+        //update our files
+        saveItems()
+        
+        //make the row color fade after selection
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -63,7 +59,7 @@ class TodoListViewController: UITableViewController {
         //create an alert controller
         let alert = UIAlertController(title: "Add New Todoey Item", message: " ", preferredStyle: .alert)
         //create an action within the alert box. the Add item button
-
+        
         //make a textfield() method variable to pass the text out of the addTextField function
         var textField = UITextField()
         
@@ -73,25 +69,47 @@ class TodoListViewController: UITableViewController {
             //set up the global variable to copy what is going to be typed later.
             textField=alertTextField
         }
-
+        
         //make the add item button to add the item to the listArray
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            
+            //instanciate an object
             let newItem = Item()
             
+            //set the title property of the object
             newItem.title = textField.text!
             
+            //add the object to the array of objects.. (list)
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
-            //what will happen once the user clicks the add item button on our ui alert
+            //update our property list
+            self.saveItems()
         }
-
-        
         alert.addAction(action)
         present(alert, animated: true ,completion: nil)
     }
+    
+    
+    //MARK - Model Manipulation Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error endcoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
 }
-
